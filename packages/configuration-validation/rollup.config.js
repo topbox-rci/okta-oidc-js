b9aea3c429d4ddfa17814d874f5ae2709443012c
Oleksandr Pravosudko <oleksandr.pravosudko@okta.com>
2020-12-16 14:14:58 +0200

    create esm bundle with rollup

---
+++ packages/configuration-validation/rollup.config.js
@@ -0,0 +1,25 @@
+import pkg from './package.json'
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: 'src/lib.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    })
+  ]
+}
